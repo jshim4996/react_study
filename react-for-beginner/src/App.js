663228694019c6ce043eb989244f5cfa1dfab4ab
joon shim <joonshim@joonui-MacBookPro.local>
2023-08-10 18:47:26 +0900

    리액트 영화 앱 6.0  ~ 6.1 강좌 학습

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,11 +1,23 @@
+import { render } from "vue";
 import Button from "./Button";
-import styles from "./App.module.css";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only one");
+  // };
+
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
   return (
     <div className="App">
-      <h1 className={styles.title}>리액트 학습</h1>
-      <Button text={"컨티뉴"}></Button>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
