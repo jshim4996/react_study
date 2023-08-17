040cb1e7faba2d3cf6c86f94dbd20ecd58fcbe5b
joon shim <jshim4996@gmail.com>
2023-08-17 09:39:46 +0900

    리액트 마스터 강의 2 완강

--- react-master-typescript/src/App.tsx
+++ react-master-typescript/src/App.tsx
@@ -1,12 +1,51 @@
-import Circle from "./Circle";
+// import Circle from "./Circle";
+import React, { useState } from "react";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <Title>Hello</Title>
+    </Container>
   );
 }
+
+// const [value, setValue] = useState("");
+
+// const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+/*   react 에선 target 대신 curre ntTarget 을 사용한다.
+     console.log(event.currentTarget.value);*/
+//   const {
+//     currentTarget: { value },
+//   } = event;
+//   setValue(value);
+// };
+
+// const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//   event.preventDefault();
+//   console.log("hello", value);
+// };
+
+/*  typeScirpt 기초 학습 3.0 ~ 3.5 학습 */
+// <form onSubmit={onSubmit}>
+//   <input
+//     value={value}
+//     onChange={onChange}
+//     type="text"
+//     placeholder="user name"
+//   />
+//   <button>Log in</button>
+// </form>
+
+/*  typeScirpt 기초 학습 3.0 ~ 3.4 */
+// <Circle borderColor="black" bgColor="teal" />
+// <Circle bgColor="tomato" text="곽예지" />
 
 export default App;
--- react-master-typescript/src/index.tsx
+++ react-master-typescript/src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 // @types 는 아직 tyeps 를 적용시키지 않은 패키지를 Js 에서 엔징에서 사용할수 있도록 지원 해주는 커뮤니티
 //TypeScirpt 는 코드가 실행하기전에 데이터 타입, 파라메터등을 확인하여 알려준다
 root.render(
-  // <ThemeProvider>
-  <App />
-  // </ThemeProvider>
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
 );
