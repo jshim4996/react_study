db9a998f9ecd7daf1767162eaf0461474c7ab368
joon shim <jshim4996@gmail.com>
2023-08-18 12:29:44 +0900

    리액트 마스터 과정 ~5.6 진행

--- react-master-typescript/src/App.tsx
+++ react-master-typescript/src/App.tsx
@@ -1,21 +1,89 @@
 // import Circle from "./Circle";
-import React, { useState } from "react";
-import { styled } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
+//createGlobalStyle 전역 스코프에 스타일을 적용 시켜줌
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
+ html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+*{
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Oswald', sans-serif;
   background-color: ${(props) => props.theme.bgColor};
-`;
-const Title = styled.h1`
   color: ${(props) => props.theme.textColor};
+
+}
+a{
+  text-decoration: none;
+  color: inherit
+  }
 `;
 
 function App() {
+  /* Fragment : react 에서 제공하는  부모 없이 서로 붙어 있는 컴포넌트를 붙어 있게 해주는 역활을 한다 없다면 불필요한 div 가 생성 될수 있다.*/
   return (
-    <Container>
-      <Title>Hello</Title>
-    </Container>
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
   );
 }
+
+/* 리액트 마스터 3.6 과정 */
+// const Container = styled.div`
+//   background-color: ${(props) => props.theme.bgColor};
+// `;
+// const Title = styled.h1`
+//   color: ${(props) => props.theme.textColor};
+// `;
+
+// <Container>
+//   <Title>Hello</Title>
+// </Container>
 
 // const [value, setValue] = useState("");
 
--- react-master-typescript/src/index.tsx
+++ react-master-typescript/src/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
+import { theme } from "./theme";
+
+// import { lightTheme, darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +13,7 @@ const root = ReactDOM.createRoot(
 // @types 는 아직 tyeps 를 적용시키지 않은 패키지를 Js 에서 엔징에서 사용할수 있도록 지원 해주는 커뮤니티
 //TypeScirpt 는 코드가 실행하기전에 데이터 타입, 파라메터등을 확인하여 알려준다
 root.render(
-  <ThemeProvider theme={darkTheme}>
+  <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
 );
